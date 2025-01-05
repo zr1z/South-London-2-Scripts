4f170fee31afff97c7788ec0486dec94eb32e383
zr1z <106442393+zr1z@users.noreply.github.com>
2025-01-05 18:12:19 +0000

    shared integ

--- callOthers.lua
+++ callOthers.lua
@@ -2,21 +2,14 @@ local Players = game:GetService("Players");
 local ReplicatedStorage = game:GetService("ReplicatedStorage");
 local CallEvent = ReplicatedStorage:WaitForChild("Call");
 
-local callingStatus = false -- false = off, true = on
+shared.toggle = true;
 
 for _, v in ipairs(Players:GetPlayers()) do
-    if toggle then
-        local callStart = {
-            [1] = v,
-            [2] = "Starting"
-        }
-
-        local callEnd = {
-            [1] = v,
-            [2] = "EndCall"
-        }
+    if shared.toggle then
+        local callStart = {v, "Starting"}
+        local callEnd = {v, "EndCall"}
         
         CallEvent:FireServer(unpack(callStart))
-        CallEvent:FireServer(unpack(callEnd))
+        -- CallEvent:FireServer(unpack(callEnd))
     end
 end
